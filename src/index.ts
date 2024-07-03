5fdd60e360b3a6499a855c283fc9fb7a792f2cad
Chris Cockburn <christophercockburn1@gmail.com>
2024-07-03 22:21:49 +0100

    chore: Update package.json version to 1.2.0 and fix typescript declaration file name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,57 @@
-function hello(): string {
-	return "Hello World"
+/**
+ * Calculates the GC content (percentage) of a DNA sequence.
+ */
+function calculateGCContent(dna: string): number {
+	const gcCount = (dna.match(/[gc]/gi) || []).length;
+	return (gcCount / dna.length) * 100;
 }
 
-module.exports = hello
+/**
+ * Transcribes DNA sequence to RNA (replacing all thymine 'T' with uracil 'U').
+ */
+function transcribeDNAtoRNA(dna: string): string {
+	return dna.replace(/T/g, 'U');
+}
+
+/**
+ * Creates the complementary DNA strand for a given DNA sequence.
+ */
+function createComplementDNA(dna: string): string {
+	const complement = {
+		'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'
+	};
+	return dna.split('').map(nucleotide => complement[nucleotide] || nucleotide).join('');
+}
+
+/**
+ * Simulates a Polymerase Chain Reaction (PCR) by amplifying DNA between primers.
+ */
+function simulatePCR(dna: string, primer1: string, primer2: string): string {
+	const start = dna.indexOf(primer1) + primer1.length;
+	const end = dna.indexOf(primer2, start);
+	return dna.substring(start, end);
+}
+
+/**
+ * Randomly mutates a DNA sequence at a specified mutation rate (probability).
+ */
+function mutateDNA(dna: string, mutationRate: number): string {
+	return dna.split('').map(nucleotide => Math.random() < mutationRate ? mutateNucleotide(nucleotide) : nucleotide).join('');
+}
+
+function mutateNucleotide(nucleotide: string): string {
+	const bases = ['A', 'T', 'C', 'G'];
+	let newBase = bases[Math.floor(Math.random() * bases.length)];
+	while (newBase === nucleotide) { // Ensure new nucleotide is different
+		newBase = bases[Math.floor(Math.random() * bases.length)];
+	}
+	return newBase;
+}
+
+module.exports = {
+	calculateGCContent,
+	transcribeDNAtoRNA,
+	createComplementDNA,
+	simulatePCR,
+	mutateDNA
+};
